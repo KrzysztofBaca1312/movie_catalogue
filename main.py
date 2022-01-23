3c2fd1fc438e4c8a6da12c3d48927ff2d43f8adb
Krzysztof Baca <baca.krzysztof@wp.pl>
2022-01-23 20:27:34 +0100

    commit3

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 from flask import Flask, render_template
 import tmdb_client
 import main as main
+from flask import request
 
 app = Flask(__name__)
 type_list = ["popular", "now_playing", "top_rated", "upcoming"]
 
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_movies(how_many=8)
-    return render_template("homepage.html", movies=movies)
+    selected_list = request.args.get('list_type', "popular")
+    movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
+    return render_template("homepage.html", movies=movies, current_list=selected_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-@app.route('/')
-def homepage():
-     movies = tmdb_client.get_popular_movies()["results"][:8]
-     return render_template("homepage.html", movies=movies)
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+   details = tmdb_client.get_single_movie(movie_id)
+   cast = tmdb_client.get_single_movie_cast(movie_id)
+   return render_template("movie_details.html", movie=details, cast=cast)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
--- tmdb_client.py
+++ tmdb_client.py
@@ -4,6 +4,8 @@ from flask import app
 import requests
 import tmdb_client
 
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTk1ZWUxNzYxODRkYzNmODY1MGE3Njg2YjkyZTYzMCIsInN1YiI6IjYxZTk5MjM5ZGYyOTQ1MDA4ZWZhNTRjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u6GdwiYemVXOZjGJhmRD7_rLRou9JXJD5Fo-CH1VvtM"
+
 def get_popular_movies():
     endpoint = "https://api.themoviedb.org/3/movie/popular"
     api_token = "twoj-token"
@@ -23,6 +25,31 @@ def utility_processor():
         return tmdb_client.get_poster_url(path, size)
     return {"tmdb_image_url": tmdb_image_url}
 
-def get_movies(how_many):
-    data = get_popular_movies()
+def get_movies(how_many, list_type):
+    data = get_popular_movies(list_type)['results']
     return data["results"][:how_many]
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"]
+
+def get_movies_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
